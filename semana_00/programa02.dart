4fd805c5eb2ea85a03650118f53b90da082226e7
LIZBETH ORTEGA <65843684+LizbethOD@users.noreply.github.com>
2021-08-09 22:17:30 +0000

    programa02

---
+++ semana_00/programa02.dart
@@ -0,0 +1,17 @@
+void main(){
+  Map <String, dynamic> persona = {
+  "nombre" : "Dejah",
+  "edad" : 25,
+  "soltero" : true, 
+  };
+
+  print(persona["edad"]);
+  persona["edad"] = 20;
+  print(persona);
+
+  persona.addAll({"email" : "dejah@correo.com"}); 
+  print(persona);
+
+  persona.remove("soltero");
+  print(persona);
+}
